df86b425cc9222ff2c3aff9a4c4fa8df22094fb1
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-07 06:35:44 +0300

    Add method for encryption

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -2,6 +2,13 @@ class Decrypt {
     private String userText;
     private int key;
     private String decryptedText;
+
+    public Decrypt(String userText, int key){
+        this.userText = userText;
+        this.key = key;
+
+
+    }
    /* public static void main(String[] args) {
         String text = "Kh|/#krz*v#lw#kdqjlqjB";
         int key = 3;
--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -10,7 +10,15 @@
 
 
     }
-
+    public static void encryption(){
+        String userText;
+        int key;
+        char [] chars = userText.toCharArray();
+        for(char c: chars){
+            c+=key;
+            System.out.println(c);
+        }
+    }
    /* public static void main(String[] args) {
         String text = "Hey, how's it hanging?";
         int key = 3;
