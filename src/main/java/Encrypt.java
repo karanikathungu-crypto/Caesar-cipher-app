beab501535bca729fde573027b5d9a775f3f0351
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-07 08:17:54 +0300

    Add gitignore file

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,34 +1,4 @@
- class Encrypt {
+package PACKAGE_NAME;
 
-    private String userText;
-    private int key;
-    private String encryptedText;
-
-    public Encrypt(String userText, int key){
-        this.userText = userText;
-        this.key = key;
-
-
-    }
-    public static void encryption(){
-        String userText;
-        int key;
-        char [] chars = userText.toCharArray();
-        for(char c: chars){
-            c+=key;
-            System.out.println(c);
-        }
-    }
-   /* public static void main(String[] args) {
-        String text = "Hey, how's it hanging?";
-        int key = 3;
-        char [] chars = text.toCharArray();
-        for(char c: chars ){
-            c += key;
-            System.out.print(c);
-
-        }
-
-
-    }*/
+public class Encrypt {
 }
